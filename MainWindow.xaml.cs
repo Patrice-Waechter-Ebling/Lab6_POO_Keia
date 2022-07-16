ac3cfa1a1b104cfabedbdec115b91062b7a9a365
Patrice <99949014+Patrice-Waechter-Ebling@users.noreply.github.com>
2022-07-16 08:19:25 -0400

    Lab6POO_Keia
    Mise a jour code source

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -13,16 +13,42 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Lab6Keia
+namespace KeiaLab6
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        Generateur gen = new Generateur();
         public MainWindow()
         {
             InitializeComponent();
+            AjouterLogo();
         }
+        /// <summary>
+        /// Cette fonction charge le fichier image du logo directement dans le dossier images
+        /// et lè envoie au controle image nommèe Logo
+        /// </summary>
+        private void AjouterLogo()
+        {
+            Image im = new Image();
+            string fichierImage = Environment.CurrentDirectory+ "\\images\\logo.jpg";
+            im.Source = new BitmapImage(new Uri(fichierImage, UriKind.Absolute));
+            Logo.Source = im.Source;
+        }
+        /// <summary>
+        /// Genere le ID puis l
+        /// </summary>
+        private void CompositionID()
+        {
+            for(int a=0; a<gen.GetDesignationType(); a++)
+            {
+                for (int b = 0; b < 10; b++) {
+                    ListeID.Items.Add($"{gen.GetDesignationType(a)}00{Math.Floor(Math.Pow(Math.PI, b))}");
+                }
+            }
+        }
+
     }
 }
